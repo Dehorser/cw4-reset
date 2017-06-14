6484edb2d23257734c72532277dcf3faaecc9c74
Yuzhou Huang <happyjoe2008@gmail.com>
2017-06-14 13:26:01 -0500

    Scene stuff

--- Chrastil Warren framework/Union/Assets/Scripts/MainMenu.cs
+++ Chrastil Warren framework/Union/Assets/Scripts/MainMenu.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
-public class SceneChange : MonoBehaviour {
+public class MainMenu : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		SceneManager.LoadSceneAsync ("Resetting Learning Phase");
 	}
 	
 	// Update is called once per frame
