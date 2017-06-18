aebc80337a5b8a71486fe95979da6045957194cd
Joe Huang <happyjoe2008@gmail.com>
2017-06-18 11:49:33 -0500

    union

--- Chrastil Warren framework/Union/Assets/MainMenu.cs
+++ Chrastil Warren framework/Union/Assets/MainMenu.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+	private static string[] learningScenes = {"Resetting Practice Phase", 
+		"CW4 Practice Phase"
+	};
+
 	// Use this for initialization
 	void Start () {
-	
+		Learning ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Learning() {
+		foreach (string s in learningScenes) {
+			StartCoroutine(SceneTimer (s, 10f));
+		}
+	}
+
+	IEnumerator SceneTimer (string sceneName, float seconds)
+	{
+		yield return new WaitForSeconds (seconds);
+		SceneManager.LoadScene (sceneName, LoadSceneMode.Single);
 	}
 }
+	
