3706871b5a1161c4fd1f83b66618ce3189689f5d
Joe Huang <happyjoe2008@gmail.com>
2017-06-19 15:48:49 -0500

    menu working

--- Chrastil Warren framework/Union/Assets/MainMenu.cs
+++ Chrastil Warren framework/Union/Assets/MainMenu.cs
@@ -1,15 +1,76 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
 
+	public Canvas menuTemplate;
+	private Canvas curMenu;
+
+	private Text prompt;
+	private Button resetting;
+	private Button CW4;
+
+	private string practice;
+	private string test;
+
 	// Use this for initialization
 	void Start () {
-        StartCoroutine(SceneTimer("Resetting Learning Phase", 2, 5));
-        StartCoroutine(SceneTimer("CW4 Learning Phase", 9, 5));
+		InitMenu ();
+        
     }
-	
+
+	void InitMenu() {
+		curMenu = Instantiate (menuTemplate);
+
+		prompt = curMenu.transform.Find ("Prompt").gameObject.GetComponent<Text>();
+
+		resetting = curMenu.transform.Find ("Resetting").gameObject.GetComponent<Button>();
+
+		CW4 = curMenu.transform.Find ("CW4").gameObject.GetComponent<Button>();
+
+		ChoosePractice ();
+	}
+
+	void ChoosePractice() {
+		prompt.text = "Choose Practice";
+
+		resetting.onClick.AddListener(() => {
+			practice = "Resetting Practice Phase";
+			ChooseTest();
+		});
+
+		CW4.onClick.AddListener (() => {
+			practice = "CW4 Practice Phase";
+			ChooseTest();
+		});
+	}
+
+	void ChooseTest() {
+		resetting.onClick.RemoveAllListeners ();
+		CW4.onClick.RemoveAllListeners ();
+
+		prompt.text = "Choose Test";
+
+		resetting.onClick.AddListener(() => {
+			test = "Resetting Test Phase";
+			StartExperiment();
+		});
+
+		CW4.onClick.AddListener (() => {
+			test = "CW4 Test Phase";
+			StartExperiment();
+		});
+	}
+
+	void StartExperiment() {
+		Destroy (curMenu);
+		StartCoroutine(SceneTimer("Resetting Learning Phase", 2, 5));
+		StartCoroutine(SceneTimer("CW4 Learning Phase", 9, 5));
+		StartCoroutine (SceneTimer (practice, 16, 5));
+		StartCoroutine (SceneTimer (test, 23, 5));
+	}
 
 	IEnumerator SceneTimer (string sceneName, float startTime, float duration)
 	{
